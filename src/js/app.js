d5ed15ee0fc2fb53ce2ba424ddc111591d3079f0
amitbm123 <amitbenm@post.bgu.ac.il>
2018-12-17 17:17:29 +0200

    finish

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,23 @@
 import $ from 'jquery';
 import {parseCode} from './code-analyzer';
-import {arrayTable} from './code-analyzer';
+//import {arrayTable} from './code-analyzer';
 import {parseb} from './code-analyzer';
-import {cleanTable} from './code-analyzer';
-import {getStringTable,setStringCode} from './code-analyzer';
+//import {cleanTable} from './code-analyzer';
+import {getStringTable,setStringCode,setArgs, getvars,getsubstring, getglobals} from './code-analyzer';
 
 
 $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
         let stringCode = $('#codePlaceholder').val();
+        let args = $('#enterArguments').val();
         setStringCode(stringCode);
+        setArgs(args);
         let parsedCode = parseCode(stringCode);
         parseb(parsedCode.body);
-        let table=getStringTable();
-        $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
-        $('#parseTable').append(table);
-        cleanTable(arrayTable);
+        $('#parsedCodeOut').html(getsubstring());
+        //let table=getStringTable();
+        // $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
+        // $('#parseTable').append(table);
+        //  cleanTable(arrayTable);
     });
 });
